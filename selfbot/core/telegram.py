af5701f52e5c169bab29d57c6c78072e1674bc9a
Beep Sound <219347502+beepsound@users.noreply.github.com>
2025-09-07 02:17:57 +0700

    perf(idle): use `asyncio.Event` instead of sleep task to reduce idle overhead (#17)
    * fix(selfbot.core.telegram): forgot to import `contextlib`
    
    * chore(selfbot): add `loop.call_soon` to ensure graceful loop termination
    
    * style: use `list` unpacking instead of `tuple` for consistency
    
    ---------
    
    Co-authored-by: n/a <n/a>

--- selfbot/core/telegram.py
+++ selfbot/core/telegram.py
@@ -1,5 +1,6 @@
 import abc
 import asyncio
+import contextlib
 import functools
 import os
 import pathlib
@@ -77,30 +78,28 @@ class Telegram(abc.ABC):
             self.config.pop(cred, None)
 
     async def idle(self) -> None:
-        if self.__idle__ and not self.__idle__.done():
+        if self.__idle__ and not self.__idle__.is_set():
             raise RuntimeError("Selfbot Idling")
 
         signames = (signal.SIGINT, signal.SIGTERM, signal.SIGABRT)
 
         def sighandler(signum: int) -> None:
             if self.__idle__:
-                self.__idle__.cancel()
+                self.__idle__.set()
 
         for signame in signames:
             self.loop.add_signal_handler(
                 signame, functools.partial(sighandler, signame)
             )
 
-        while True:
-            self.__idle__ = self.loop.create_task(asyncio.sleep(900))
+        self.__idle__ = asyncio.Event()
 
-            try:
-                await self.__idle__
-            except asyncio.CancelledError:
-                break
-
-        for signame in signames:
-            self.loop.remove_signal_handler(signame)
+        try:
+            await self.__idle__.wait()
+        finally:
+            for signame in signames:
+                with contextlib.suppress(Exception):
+                    self.loop.remove_signal_handler(signame)
 
     def updates(self) -> None:
         fltapp = flt.user(self.app.me.id)
@@ -139,12 +138,12 @@ class Telegram(abc.ABC):
             "is_bot",
         ]
         creds = await asyncio.gather(
-            *(getattr(client.storage, attr)() for attr in attrs)
+            *[getattr(client.storage, attr)() for attr in attrs]
         )
 
         await storage.open()
         await asyncio.gather(
-            *(getattr(storage, attr)(cred) for attr, cred in zip(attrs, creds))
+            *[getattr(storage, attr)(cred) for attr, cred in zip(attrs, creds)]
         )
 
     @property
